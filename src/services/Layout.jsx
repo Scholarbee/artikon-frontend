6b176d7a9faaca2a4243e8b324106d3e8b5c89f2
Scholarbee <scholarbee6@gmail.com>
2024-08-16 03:51:26 +0000

    bg

--- src/services/Layout.jsx
+++ src/services/Layout.jsx
@@ -1,26 +1,33 @@
 /* eslint-disable react/display-name */
-import { Box, Container } from "@mui/material";
+import { Box } from "@mui/material";
 import React from "react";
 import Navbar from "../components/global/Navbar";
 import Sidebar from "../components/global/Sidebar";
 
 const Layout =
   (Component) =>
-    ({ ...props }) => {
-    
+  ({ ...props }) => {
     return (
-      <>
-          <div style={{ display: "flex", minHeight: "100vh" }}>
-            <Sidebar />
-            <Box sx={{ width: "100%", bgcolor: "rgb(232, 232, 232)" }}>
-              <Navbar />
-              <Box sx={{ p: 2 }}>
-                <Component {...props} />
-              </Box>
-            </Box>
-          </div>
-      </>
+      <div style={{ display: "flex", minHeight: "100vh" }}>
+        <Sidebar />
+        <Box sx={{ flexGrow: 1, bgcolor: "rgb(232, 232, 232)" }}>
+          <Navbar />
+          <Box
+            sx={{
+              p: 2,
+              backgroundImage: "url('/bg3.jpg')",
+              backgroundSize: "cover",
+              minHeight: "100vh",
+              backgroundAttachment: "fixed", // Ensures the background image stays fixed while scrolling
+              backgroundPosition: "center",
+            }}
+          >
+            <Component {...props} />
+          </Box>
+        </Box>
+      </div>
     );
   };
 
 export default Layout;
+
